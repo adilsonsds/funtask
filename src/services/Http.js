f3fad3437c747331257645ee39cf837fefb59b1f
Adilson Sousa <adilsonsdesousa@gmail.com>
2018-10-20 13:24:48 -0300

    Adicionar versionamento da API

--- src/services/Http.js
+++ src/services/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://localhost:5001/api/',
+  baseURL: 'https://localhost:5001/api/v1/',
   timeout: 60000,
   headers: {
     'X-Custom-Header': 'foobar',
