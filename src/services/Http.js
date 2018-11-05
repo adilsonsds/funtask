b2f6d0dc2f9a3192b7e0412ed6ae2149083550af
Adilson Sousa <adilsonsdesousa@gmail.com>
2018-11-04 23:03:59 -0200

    Alterar baseURL

--- src/services/Http.js
+++ src/services/Http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://localhost:5001/api/v1/',
+  baseURL: 'https://funtask-server.herokuapp.com/api/v1/',
   timeout: 120000,
   headers: {
     // 'Authorization': authorizationHeader()
