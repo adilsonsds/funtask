ca8f63bc0fc0e79ac4c8f06c339c296c0b209341
Adilson Sousa <adilsonsdesousa@gmail.com>
2018-11-11 16:05:09 -0200

    Alterações visuais no tela de avaliações

--- src/modules/case/pages/LicaoAvaliacao.vue
+++ src/modules/case/pages/LicaoAvaliacao.vue
@@ -1,66 +1,56 @@
 <template>
     <div class="container">
-        <form @submit.prevent="filtrarQuestoes" class="mb-3">
-            <div class="row mb-2">
-                <div class="col">
-                    <!-- <router-link class="h3 d-block mb-3" :to="{ name: 'case-licoes', params: { id: idCase }}" >{{ nomeCase }}</router-link> -->
-                    <h5>Lição: {{ titulo }}</h5>
-                    <h6 v-if="!!dataLiberacao">Liberada em: {{ dataLiberacao }}</h6>
-                    <h6 v-if="!!dataEncerramento">Termina em: {{ dataEncerramento }}</h6>
-                </div>
+        <div class="row mb-2">
+            <div class="col">
+                <!-- <router-link class="h3 d-block mb-3" :to="{ name: 'case-licoes', params: { id: idCase }}" >{{ nomeCase }}</router-link> -->
+                <h5>Lição: {{ titulo }}</h5>
+                <h6 v-if="!!dataLiberacao">Liberada em: {{ dataLiberacao }}</h6>
+                <h6 v-if="!!dataEncerramento">Termina em: {{ dataEncerramento }}</h6>
             </div>
-            <div class="row">
-              <div class="col">
-                <div class="card">
-                  <div class="card-body">
-                    <pre>{{ textoApresentacao }}</pre>
+        </div>
+        <form @submit.prevent="filtrarQuestoes" class="my-3">
+          <div class="form-row align-items-center">
+            <div class="col-md-3 my-1">
+              <label class="mr-sm-2 sr-only" for="filtro-numero-questao">Questão</label>
+              <select class="custom-select mr-sm-2" id="filtro-numero-questao" v-model="filtro.idQuestao" required>
+                <option v-for="(questao, index) in questoes" :key="questao.idQuestao" v-bind:value="questao.idQuestao">{{ index + 1 }}</option>
+              </select>
+            </div>
+            <div class="col-md-9 my-1">
+              <div class="custom-control custom-checkbox mr-sm-2">
+                <input type="checkbox" v-model="filtro.removerQuestoesJaAvaliadas" class="custom-control-input" id="filtro-questoes-respondidas">
+                <label class="custom-control-label" for="filtro-questoes-respondidas">Remover questões que já foram avaliadas</label>
+              </div>
+            </div>
+          </div>
+          <div class="form-row align-items-center my-3">
+            <div class="col">
+              <button type="submit" class="btn btn-primary">Filtrar</button>
+            </div>
+          </div>
+        </form>
+        <div class="card my-4" v-for="resposta in respostas" :key="resposta.idResposta">
+          <div class="card-header">
+            Resposta
+          </div>
+          <div class="card-body">
+            <pre>{{ resposta.resposta }}</pre>
+          </div>
+          <form @submit.prevent="atribuirPontuacao(resposta)" class="card-footer text-muted">
+            <div class="form-row">
+              <div class="col-md-4">
+                <div class="input-group mb-3">
+                  <input type="number" class="form-control" placeholder="Nota" v-model="resposta.pontos" v-bind:id="'nota-resposta-'+resposta.idResposta" maxlength="10" required>
+                  <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="atribuir-nota">Atribuir Nota</button>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="form-row mt-2">
-                <div class="form-group col-lg-6 col-md-12">
-                    <label for="filtro-numero-questao">Número da Questão:</label>
-                    <select id="filtro-numero-questao" class="form-control" v-model="filtro.idQuestao" required>
-                        <option v-for="(questao, index) in questoes" :key="questao.idQuestao" v-bind:value="questao.idQuestao">{{ index + 1 }}</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-row mt-2">
-                <div class="form-group col-lg-6 col-md-6">
-                    <div class="custom-control custom-checkbox">
-                      <input type="checkbox" v-model="filtro.removerQuestoesJaAvaliadas" class="custom-control-input" id="filtro-questoes-respondidas">
-                      <label class="custom-control-label" for="filtro-questoes-respondidas">Remover questões que já foram avalidas</label>
-                    </div>
-                </div>
-            </div>
-            <div class="form-row">
-              <div class="col text-right">
-                <button type="submit" class="btn btn-success">Filtrar</button>
-              </div>
-            </div>
-        </form>
-        <form v-for="resposta in respostas" :key="resposta.idResposta" @submit.prevent="atribuirPontuacao(resposta)">  
-          <div class="row mt-2">
-            <div class="col">
-              <h3>Resposta</h3>
-              <pre>{{ resposta.resposta }}</pre>
-            </div>
-          </div>
-          <div class="row mt-2">
-            <label class="sr-only" v-bind:for="'nota-resposta-'+resposta.idResposta">Nota</label>
-            <div class="input-group mb-2 mr-sm-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">Nota</div>
-              </div>
-              <input type="number" class="form-control" v-model="resposta.pontos" v-bind:id="'nota-resposta-'+resposta.idResposta" maxlength="10" required>
-            </div>
-            <button type="submit" class="btn btn-primary mb-2">Salvar</button>
-          </div>
-        </form>
+          </form>
+        </div>
     </div>
 </template>
-
 <script>
 import {
   prepararAvaliacao,
@@ -73,7 +63,7 @@ export default {
       idCase: 0,
       idLicao: 0,
       filtro: {
-        idQuestao: 1,
+        idQuestao: '',
         removerQuestoesJaAvaliadas: true
       },
       titulo: "",
